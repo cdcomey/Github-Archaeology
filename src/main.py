b2496527988fed7caa28beb16518cfdbd798ec2e
cdcomey <charlesdcomey@gmail.com>
2025-01-08 13:20:56 -0800

    now parses timestamp for commit

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import subprocess
+import datetime
 
 def find_commit_hashes():
     result = subprocess.run(['git', 'log', '--oneline'], capture_output=True, cwd='../data/godot', text=True)
@@ -15,16 +16,32 @@ def find_file_extension(line):
     assert first_file[dot_index_a:] == last_file[dot_index_b:]
 
     return first_file[dot_index_a:]
+
+def parse_timestamp(date_str):
+    date = datetime.datetime.strptime(date_str[:-6], '%a %b %d %H:%M:%S %Y')
+
+    offset_str = date_str[-5:]
+    offset_hours = int(offset_str[:3])
+    offset_minutes = int(offset_str[0] + offset_str[3:])  # Preserve the sign for minutes
+    offset = datetime.timedelta(hours=offset_hours, minutes=offset_minutes)
+    dt_with_offset = date.replace(tzinfo=datetime.timezone(offset))
+
+    return dt_with_offset
     
 def analyze_commit(commit_hash):
     commit_timestamp = {}
     result = subprocess.run(['git', 'show', commit_hash], capture_output=True, cwd='../data/godot', text=True)
     result = result.stdout
     # print(result)
+    date = ''
     file_extension = ''
     lines_added = 0
     for line in result.split('\n'):
-        if line.startswith('diff --git a/'):
+        if line.startswith('Date:   '):
+            date_str = line[len('Date:   '):]
+            date = parse_timestamp(date_str)
+
+        elif line.startswith('diff --git a/'):
             if file_extension in commit_timestamp:
                 commit_timestamp[file_extension] += lines_added
             else:
@@ -32,23 +49,31 @@ def analyze_commit(commit_hash):
             
             file_extension = find_file_extension(line)
             lines_added = 0
+
         elif line.startswith('+'):
             lines_added += 1
         elif line.startswith('-'):
             lines_added -= 1
     
     del commit_timestamp['']
-    return commit_timestamp
+    return date, commit_timestamp
+
+def add_timestamp_to_histories(language_histories, commit_timestamp):
+    pass
 
 def main():
     language_histories = []
 
     hashes = find_commit_hashes()
-    # print(hashes[1000])
-    l = analyze_commit(hashes[1000])
-    print(l)
-    # for h in hashes:
-    #     print(h)
-
+    date, commit_info = analyze_commit(hashes[1000])
+    print(date)
+    print(commit_info)
+    
 if __name__ == '__main__':
     main()
+
+# list of dicts
+# 	key = lang name
+# 	val = sorted dict
+# 		keys = times
+# 		vals = num lines
