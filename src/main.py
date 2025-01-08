473dadc1554b1d5d1d21768b66ab9f1dd06ee2df
cdcomey <charlesdcomey@gmail.com>
2025-01-07 18:58:23 -0800

    now counts lines added in a particular commit

--- src/main.py
+++ src/main.py
@@ -6,11 +6,26 @@ def find_commit_hashes():
     # print(result)
     hashes = [r[:r.find(' ')] for r in result if ' ' in r]
     return hashes
+    
+def analyze_commit(commit_hash):
+    result = subprocess.run(['git', 'show', commit_hash], capture_output=True, cwd='../data/godot', text=True)
+    result = result.stdout
+    lines_added = 0
+    for line in result.split('\n'):
+        if line.startswith('+'):
+            lines_added += 1
+        elif line.startswith('-'):
+            lines_added -= 1
+    
+    return lines_added
 
 def main():
     hashes = find_commit_hashes()
-    for h in hashes:
-        print(h)
+    print(hashes[0])
+    l = analyze_commit(hashes[1000])
+    print(l)
+    # for h in hashes:
+    #     print(h)
 
 if __name__ == '__main__':
     main()
