a4b13dcc0b3f1f171c2ac5432217fd81277cb3ad
cdcomey <charlesdcomey@gmail.com>
2025-01-14 00:14:14 -0800

    can now save language histories to file

--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ import subprocess
 import datetime
 from sortedcontainers import SortedDict
 import matplotlib.pyplot as plt
+import json
 
-def find_commit_hashes():
-    result = subprocess.run(['git', 'log', '--oneline'], capture_output=True, cwd='../data/godot', text=True)
+def find_commit_hashes(repo_dir):
+    result = subprocess.run(['git', 'log', '--oneline'], capture_output=True, cwd=repo_dir, text=True)
     result = result.stdout.split('\n')
     hashes = [r[:r.find(' ')] for r in result if ' ' in r]
     return hashes
@@ -30,8 +31,8 @@ def parse_timestamp(date_str):
 
     return dt_with_offset
 
-def find_file_length(hash, file):
-    result = subprocess.run(['git', 'show', f'{hash}:{file}'], capture_output=True, cwd='../data/godot', text=True)
+def find_file_length(hash, file, repo_dir):
+    result = subprocess.run(['git', 'show', f'{hash}:{file}'], capture_output=True, cwd=repo_dir, text=True)
     if result.returncode == 0:
         return len(result.stdout.splitlines())
     else:
@@ -44,11 +45,11 @@ def add_to_dict(dict, key, val):
         dict[key] = val
 
 # parse the diff text of a commit and return the line count changes for each file type
-def analyze_commit(commit_hash):
+def analyze_commit(commit_hash, repo_dir):
     print('showing', commit_hash)
 
     # get the commit text
-    result = subprocess.run(['git', 'show', '--no-merges', commit_hash], capture_output=True, cwd='../data/godot')
+    result = subprocess.run(['git', 'show', '--no-merges', commit_hash], capture_output=True, cwd=repo_dir)
     result = result.stdout.decode('utf-8', errors='ignore')
 
     date = ''
@@ -65,7 +66,7 @@ def analyze_commit(commit_hash):
         elif line.startswith('diff --git a/'):
             file1, file_extension1, file2, file_extension2 = parse_file_names(line)
             if file_extension1 != file_extension2:
-                old_file_length = find_file_length(commit_hash, file2)
+                old_file_length = find_file_length(commit_hash, file2, repo_dir)
                 add_to_dict(commit_info, file_extension1, -1*old_file_length)
                 add_to_dict(commit_info, file_extension2, old_file_length)
             
@@ -105,6 +106,22 @@ def print_language_histories(language_histories):
             running_total += lines_added_at_timestamp
             print('\t', running_total, 'lines so far')
 
+def save_dict_to_file(language_histories, dict_dir):
+    formatted_language_histories = {}
+    for lang, v in language_histories.items():
+        formatted_language_histories[lang] = {str(time):line_counts for (time, line_counts) in zip(v.keys(), v.values())}
+    with open(dict_dir, 'w') as file:
+        json.dump(formatted_language_histories, file, indent=4)
+    
+
+def import_json_as_dict(dict_dir):
+    language_histories = {}
+    with open(dict_dir, 'r') as file:
+        formatted_language_histories = json.load(file)
+    for lang, v in formatted_language_histories.items():
+        language_histories[lang] = SortedDict({datetime.datetime.fromisoformat(time):line_counts for (time, line_counts) in zip(v.keys(), v.values())})
+    return language_histories
+
 def data_grapher(language_histories):
     threshold = 100
     for lang in language_histories.keys():
@@ -127,15 +144,23 @@ def data_grapher(language_histories):
 
 def main():
     language_histories = {}
+    repo_dir_partial = 'godot'
+    repo_dir = '../data/' + repo_dir_partial
+    json_filename = '../data/dicts/' + repo_dir_partial + '.json'
 
     # list is reversed to place in chronological order
-    hashes = find_commit_hashes()
+    hashes = find_commit_hashes(repo_dir)
     hashes.reverse()
-    for hash in hashes:
-        date, commit_info = analyze_commit(hash)
+    for hash in hashes[:100]:
+        date, commit_info = analyze_commit(hash, repo_dir)
         language_histories = add_timestamp_to_histories(language_histories, date, commit_info)
-    # print_language_histories(language_histories)
+    print_language_histories(language_histories)
     data_grapher(language_histories)
+
+    save_dict_to_file(language_histories, json_filename)
+    # language_histories = import_json_as_dict(json_filename)
+    # print_language_histories(language_histories)
+    # data_grapher(language_histories)
 
 if __name__ == '__main__':
     main()
